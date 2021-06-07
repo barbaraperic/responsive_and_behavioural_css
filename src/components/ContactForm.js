84dabacdccbeb94c98140953b7ffd951398ea0c4
Barbara Peric <b.peric10@gmail.com>
2021-06-07 12:56:23 +0200

    FEAT: Contact Form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,12 +2,23 @@ import styled from 'styled-components';
 
 const ContactForm = () => {
   return (
-    <Wrapper>
-      <Field label="Name"/>
-    </Wrapper>
+    <form>
+      <Field id="name" label="Name" placeholder="Your name"/>
+      <Field 
+        id="email" 
+        label="Email"
+        type="email"
+        placeholder="mary.jackson@gmail.com"
+      />
+      <Field 
+        id="message"
+        label="Message"
+        as="textarea"
+      />
+      <Button>Submit</Button>
+    </form>
   )
 }
-
 
 const Field = ({ id, as, label, ...delegated }) => {
   const inputHeight = as === 'textarea' ? '140' : 'undefined';
@@ -26,19 +37,32 @@ const Field = ({ id, as, label, ...delegated }) => {
 }
 
 const FieldWrapper = styled.div`
-
+  display: flex;
+  align-items: baseline;
+  gap: 8px;
+  padding: 4px;
 `
 
 const FieldLabel = styled.label`
   max-width: 80px;
+  flex: 2;
 `
 
 const Input = styled.input`
   height: var(--input-height);
+  flex: 3;
+  outline-offset: 4px;
+  min-width: 80px;
+  font-size: 1rem;
+  padding: 4px;
 `
 
-const Wrapper = styled.div`
-
+const Button = styled.button`
+  display: block;
+  font-size: 1rem;
+  margin: 32px auto 0;
+  padding: 8px 24px;
+  outline-offset: 8px;
 `
 
 export default ContactForm;
